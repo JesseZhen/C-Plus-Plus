8cbee08f74e1e2107f07a837f586bf27d6a6d13e
JesseZhen <jesse.zhen@outlook.com>
2017-01-17 16:21:03 +0800

    Create car.h

---
+++ class-base-define/car.h
@@ -0,0 +1,18 @@
+#ifndef _CAR_H_
+#define _CAR_H_
+
+class Car {
+public:
+	Car(int price,int carNum);//声明构造函数
+	void run();
+	void stop();
+	void setProperty(int price, int carNum);
+	void print();
+private:
+	int m_price;
+	int m_carNum;
+};
+
+
+
+#endif
